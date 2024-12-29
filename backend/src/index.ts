c44446174fa78c5175539e3ef3ed6c330946773d
Ravi0529 <150603739+Ravi0529@users.noreply.github.com>
2024-12-29 23:12:28 +0530

    bug fixes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,12 @@
-import { WebSocketServer } from 'ws';
+import { WebSocketServer, WebSocket } from 'ws';
 import { GameManager } from './GameManager';
 
 const wss = new WebSocketServer({ port: 8080 });
 
 const gameManager = new GameManager();
 
-wss.on('connection', function connection(ws) {
+wss.on('connection', function connection(ws: WebSocket) {
   gameManager.addUser(ws);
 
-  ws.on("disconnect", () => gameManager.removeUser(ws));
+  ws.on("close", () => gameManager.removeUser(ws));
 });
